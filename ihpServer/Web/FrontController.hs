1f8bdb05cd6b6119917265c26c36ed348d3e3db8
Gennadi Heimann <gennadiheimann@gmail.com>
2023-03-17 16:51:40 +0100

    WebsocketController run

--- ihpServer/Web/FrontController.hs
+++ ihpServer/Web/FrontController.hs
@@ -3,6 +3,7 @@ module Web.FrontController where
 import IHP.RouterPrelude
 import Web.Controller.Prelude
 import Web.View.Layout (defaultLayout)
+import Web.Controller.Websocket
 
 -- Controller Imports
 import Web.Controller.Static
@@ -11,6 +12,7 @@ instance FrontController WebApplication where
     controllers = 
         [ startPage WelcomeAction
         -- Generator Marker
+        , webSocketApp @WebsocketController
         ]
 
 instance InitControllerContext WebApplication where
--- ihpServer/static/app.js
+++ ihpServer/static/app.js
@@ -1,3 +1,13 @@
 $(document).on('ready turbolinks:load', function () {
     // This is called on the first page load *and* also when the page is changed by turbolinks
 });
+
+var webSocketController = new WebSocket('ws://localhost:8000/WebsocketController');
+
+webSocketController.onopen = function (event) {
+    console.log('Connected');
+};
+
+webSocketController.onmessage = function (event) {
+    console.log(event.data);
+};
